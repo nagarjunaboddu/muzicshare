3ed1e43b639f9997f2cf236eb68d639b09727275
subhrajit <subhrajit.banerjee.official>
2021-02-06 09:48:14 -0500

    Initial commit

---
+++ src/main/java/com/galvanize/muzicshare/MuzicshareApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.muzicshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzicshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzicshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/muzicshare/MuzicshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.muzicshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzicshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
